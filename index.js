b6ce5cd70977ed4ef813a0ce08f594022f045ce0
fgfjfgnjg <104136695+fgfjfgnjg@users.noreply.github.com>
2022-05-19 11:51:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -434,7 +434,7 @@ client.on('ready',async () => {
     console.log("[vcodes.xyz]: Bot successfully connected as "+client.user.tag+".");
     let botsSchema = require("./src/database/models/botlist/bots.js");
     const bots = await botsSchema.find();
-    client.user.setPresence({ activity: { type: 'WATCHING', name: 'vcodes.xyz | '+bots.length+' bots' }, status: "dnd" });
+    client.user.setPresence({ activity: { type: 'WATCHING', name: 'plexbotlist.ga | '+bots.length+' bots' }, status: "online" });
 });
 /*=======================================================================================*/
 
